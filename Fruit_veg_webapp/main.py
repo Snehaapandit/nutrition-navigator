ac220cd39b48611fefe0cf7881bff8db7373b626
sneha pandit <snehapandit98765@gmail.com>
2025-04-24 23:45:15 +0530

    Fix: use absolute path for loading trained_model.h5

--- Fruit_veg_webapp/main.py
+++ Fruit_veg_webapp/main.py
@@ -80,7 +80,8 @@ nutrition_data = pd.DataFrame({
 # --- Load Model Once ---
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("trained_model.h5")
+    model_path = os.path.join(os.path.dirname(__file__), "trained_model.h5")
+    return tf.keras.models.load_model(model_path)
 
 model = load_model()
 
